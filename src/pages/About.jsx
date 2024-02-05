dd0b70bdadd43ac14cb932fd6a152a190ffb1b6f
nlad218 <nlad218@gmail.com>
2024-02-05 10:29:41 -0700

    updated bio section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -9,17 +9,17 @@ export default function About() {
       </div>
       <div className="bio">
         <p>
-          I'm a dedicated Full Stack Developer with a passion for crafting
-          innovative, user-centric web solutions. My versatile skill set spans
-          front-end and back-end technologies, allowing me to create elegant and
-          efficient applications that surpass expectations. In dynamic,
-          collaborative environments, I thrive on solving complex challenges and
-          staying at the forefront of industry trends. My unwavering commitment
-          to excellence, coupled with a genuine love for problem-solving,
-          defines my approach to web development. Each line of code I write and
-          project I undertake is a testament to my dedication to delivering
-          exceptional user experiences. I'm excited to contribute my expertise
-          to future projects and push boundaries in the ever-evolving tech
+          A dedicated Full Stack Developer with a passion for crafting
+          innovative, user-centric web solutions, possessing a versatile skill
+          set that spans front-end and back-end technologies. Specialized in
+          creating elegant and efficient applications that surpass expectations,
+          particularly in dynamic, collaborative environments. Thrives on
+          solving complex challenges and staying at the forefront of industry
+          trends, maintaining an unwavering commitment to excellence and a
+          genuine love for problem-solving. Each line of code written and
+          project undertaken is a testament to the dedication to delivering
+          exceptional user experiences. Excited to contribute expertise to
+          future projects and push boundaries in the ever-evolving tech
           landscape.
         </p>
       </div>
